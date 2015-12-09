f9aa4671abe185ac4995bb32509b588d6ce9c0a1
maxventu <1995_maxim@mail.ru>
2015-12-09 13:30:57 +0300

    Changing base

--- maximkalenik_making_base.sql
+++ maximkalenik_making_base.sql
@@ -18,6 +18,19 @@ CREATE SCHEMA IF NOT EXISTS `maximkalenik` DEFAULT CHARACTER SET utf8 ;
 USE `maximkalenik` ;
 
 -- -----------------------------------------------------
+-- Table `maximkalenik`.`location`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `maximkalenik`.`location` (
+  `postcode` VARCHAR(10) NOT NULL,
+  `country` VARCHAR(45) NULL DEFAULT NULL,
+  `city` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`postcode`),
+  UNIQUE INDEX `postcode_UNIQUE` (`postcode` ASC))
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
 -- Table `maximkalenik`.`contact`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `maximkalenik`.`contact` (
@@ -26,43 +39,30 @@ CREATE TABLE IF NOT EXISTS `maximkalenik`.`contact` (
   `last_name` VARCHAR(45) NOT NULL,
   `patronymic` VARCHAR(45) NULL DEFAULT NULL,
   `date_of_birth` DATE NULL DEFAULT NULL,
-  `sex_is_male` TINYINT(1) NULL DEFAULT b'1',
+  `sex_is_male` TINYINT(1) NULL DEFAULT '1',
   `nationality` VARCHAR(45) NULL DEFAULT NULL,
   `family_status` VARCHAR(45) NULL DEFAULT NULL,
   `web_site` VARCHAR(45) NULL DEFAULT NULL,
   `email` VARCHAR(45) NULL DEFAULT NULL,
   `current_workplace` VARCHAR(45) NULL DEFAULT NULL,
-  `photo_url` VARCHAR(255) NULL,
-  `is_deleted` TINYINT(1) NULL DEFAULT 0,
-  `date_deleted` DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
--- -----------------------------------------------------
--- Table `maximkalenik`.`address`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `maximkalenik`.`address` (
-  `contact_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `country` VARCHAR(45) NULL DEFAULT NULL,
-  `city` VARCHAR(45) NULL DEFAULT NULL,
+  `photo_url` VARCHAR(255) NULL DEFAULT NULL,
   `street` VARCHAR(45) NULL DEFAULT NULL,
-  `house` VARCHAR(45) NULL DEFAULT NULL,
-  `apartment` INT NULL DEFAULT NULL,
-  `postcode` INT NULL DEFAULT NULL,
-  `is_deleted` TINYINT(1) NULL DEFAULT 0,
+  `house` VARCHAR(10) NULL DEFAULT NULL,
+  `apartment` VARCHAR(10) NULL DEFAULT NULL,
+  `is_deleted` TINYINT(1) NULL DEFAULT '0',
   `date_deleted` DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (`contact_id`, `contact_id`),
-  UNIQUE INDEX `contact_id_UNIQUE` (`contact_id` ASC),
-  INDEX `fk_address_contact1_idx` (`contact_id` ASC),
-  CONSTRAINT `fk_address_contact1`
-    FOREIGN KEY (`contact_id`)
-    REFERENCES `maximkalenik`.`contact` (`id`)
+  `location_postcode` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `fk_contact_location1_idx` (`location_postcode` ASC),
+  CONSTRAINT `fk_contact_location1`
+  FOREIGN KEY (`location_postcode`)
+  REFERENCES `maximkalenik`.`location` (`postcode`)
     ON DELETE NO ACTION
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -74,17 +74,17 @@ CREATE TABLE IF NOT EXISTS `maximkalenik`.`attachments` (
   `date_upload` DATETIME NULL DEFAULT NULL,
   `comment` VARCHAR(100) NULL DEFAULT NULL,
   `contact_id` INT(11) NOT NULL,
-  `is_deleted` TINYINT(1) NULL DEFAULT 0,
+  `is_deleted` TINYINT(1) NULL DEFAULT '0',
   `date_deleted` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`id`, `contact_id`),
   INDEX `fk_attachments_contact_idx` (`contact_id` ASC),
   CONSTRAINT `fk_attachments_contact`
-    FOREIGN KEY (`contact_id`)
-    REFERENCES `maximkalenik`.`contact` (`id`)
+  FOREIGN KEY (`contact_id`)
+  REFERENCES `maximkalenik`.`contact` (`id`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -102,12 +102,14 @@ CREATE TABLE IF NOT EXISTS `maximkalenik`.`telephone` (
   PRIMARY KEY (`id`, `contact_id`),
   INDEX `fk_telephone_contact1_idx` (`contact_id` ASC),
   CONSTRAINT `fk_telephone_contact1`
-    FOREIGN KEY (`contact_id`)
-    REFERENCES `maximkalenik`.`contact` (`id`)
+  FOREIGN KEY (`contact_id`)
+  REFERENCES `maximkalenik`.`contact` (`id`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARACTER SET = utf8;
+
 
 CREATE USER 'mkalenik'@'localhost' IDENTIFIED BY 'mkalenik';
 GRANT UPDATE,SELECT,INSERT,EXECUTE ON maximkalenik.* TO 'mkalenik'@'localhost';
